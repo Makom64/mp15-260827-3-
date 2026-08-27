b5a5e2e44bf6223418af15e83f0aa44318d25b7b
wkdwnsk2 <wkdwnsk2@gmail.com>
2026-08-27 17:33:07 +0900

    [Feat] 노준철 호출 및 매서드 추가
    #1

---
+++ 260827/260827/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== 3조 팀원 소개 ===");
+        // 아래로 노승훈, 노준혁, 강성현 소개 '호출' 추가하기
+
+        Wkdwnsk2();
+
+
+        Console.WriteLine("=== 소개 끝 ===");
+    }
+    // 팀원별 메서드 추가하는 자리
+    public void Wkdwnsk2
+    {
+        System.Console.WriteLine("노준철 / 게임을 좋아합니다.");
+    }
+}
