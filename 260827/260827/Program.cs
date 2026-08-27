636f5e306f71f8afe01ccf18ca4de9b621130e6e
Makom64 <yellowffd400@gmail.com>
2026-08-27 17:23:34 +0900

    [Feat] 저장소 준비

---
+++ 260827/260827/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== 3조 팀원 소개 ===");
+        // 아래로 노승훈, 노준혁, 강성현 소개 '호출' 추가하기
+
+        Console.WriteLine("=== 소개 끝 ===");
+    }
+    // 팀원별 메서드 추가하는 자리
+    
+}
