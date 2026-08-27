0488e277a46235a514063a0df9c5fb67f6234641
Makom64 <yellowffd400@gmail.com>
2026-08-27 17:26:59 +0900

    [Docs] 주석 수정

--- 260827/260827/Program.cs
+++ 260827/260827/Program.cs
@@ -5,7 +5,7 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("=== 3조 팀원 소개 ===");
-        // 아래로 노승훈, 노준혁, 강성현 소개 '호출' 추가하기
+        // 아래로 노승훈, 노준혁 소개 '호출' 추가하기
 
         Console.WriteLine("=== 소개 끝 ===");
     }
